961afd25ef234d50f0ce6ada7b9b7387627db8f1
Alejandro Barrios <alejandro@aleate.com>
2021-03-22 19:59:58 -0600

    feat(users and haml): add users scaffold with haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #En este caso el PATH es 'hola', el controlador es 'welcome' y su acción es 'hello'
